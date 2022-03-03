15cc95cdd57be8436f273f0ae1307427758cb4c5
nsromma <99446305+nsromma@users.noreply.github.com>
2022-03-04 00:53:40 +0200

    Add files via upload
    In this script, I have created a simple calculator that uses only the 'addition' function. In my subsequent exploration, I'll add other operators such as division, multiplication and subtraction.

---
+++ Simple-calculator-with-only-addition-function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#*************
+#Description:Building simple calculator using scripting
+#Date:02.03.2022
+#Author:nsromma
+#**************************************************
+
+echo "Type your first number"
+
+read num1
+
+echo "Insert your second number"
+
+read num2
+
+echo Your answer is "$num1 + $num2 = $((num1+num2))"
+
+echo "Do you want to do more calculation?"
+
+exit
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+-- INSERT --                                                                                                                                           18,1          All
